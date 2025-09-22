d5a52ea2257a1e96a901db7e956c7bdb767bf24c
Eng Mohammed Aloshaibi <107276162+abusubhi@users.noreply.github.com>
2025-09-22 06:36:01 +0300

    the best commit
    .

---
+++ BarcodeReader/Models/BarcodeMaster.cs
@@ -0,0 +1,8 @@
+﻿namespace BarcodeReader.Models
+{
+    public class BarcodeMaster
+    {
+        public int Id { get; set; }
+        public string Barcode { get; set; }
+    }
+}
--- README.md
+++
@@ -1,2 +0,0 @@
-# BarcodeReader
-Web application to read list of barcodes , compare between these barcodes , the barcodes in the database and to Export the Excel Sheet 
